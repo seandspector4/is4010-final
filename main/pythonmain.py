eb61359056acfc0356aa0334e34b59099d306e39
Sean Spector <seanspector4@gmail.com>
2026-04-24 15:02:56 -0400

    First edits to Python file

---
+++ main/pythonmain.py
@@ -0,0 +1,11 @@
+"""
+IS4010 final project: Pig-Latin converter
+Author: Sean Spector
+Date: April 2026
+This program takes an English word input and converts it to Pig Latin and vice versa. The rules for Pig Latin are:
+ 1. If the words begins with a vowel, add "yay" to the end of the word.
+ 2. If the word begins with a consonant, move the first letter to the end of the word and add "ay".
+3. If the word begins with multiple consants move all the consonants to the end of the word and add "ay".
+"""
+
+
